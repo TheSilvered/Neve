0d308157ba410cd199e2b8308ddedd4c3da6b8af
Davide Taffarello <taffarello.da@gmail.com>
2025-06-04 20:08:24 +0200

    Added strNew tests

---
+++ tests/test_str/strNew.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <stdlib.h>
+
+#include "nv_test.h"
+#include "nv_str.h"
+
+void test_strNewZeroReserve(void) {
+    Str *str = strNew(0);
+    testAssertRequire(str != NULL);
+    testAssert(str->len == 0);
+    testAssert(strcmp(strAsC(str), ""));
+    free(str);
+}
+
+void test_strNewNonZeroReserve(void) {
+    size_t reserve = 10;
+    Str *str = strNew(reserve);
+    testAssertRequire(str != NULL);
+    testAssert(str->len == 0);
+    testAssert(str->cap >= reserve);
+    testAssert(strcmp(strAsC(str), ""));
+    free(str);
+}
+
+testList(
+    testMake(test_strNewZeroReserve),
+    testMake(test_strNewNonZeroReserve)
+)
