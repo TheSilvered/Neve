fc933256c6ab72ea225d13fd7f097164d0fb04f4
Davide Taffarello <taffarello.da@gmail.com>
2025-06-06 20:47:44 +0200

    Added StrView tests

---
+++ tests/test_str/strViewInitFromC.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+#include "nv_test.h"
+#include "nv_str.h"
+
+void test_strViewInitFromCEmpty(void) {
+    const char *cStr = "";
+    StrView sv;
+    strViewInitFromC(&sv, cStr);
+
+    testAssert(strcmp((char *)sv.buf, cStr) == 0);
+    testAssert(sv.len == 0);
+}
+
+void test_strViewInitFromCFull(void) {
+    const char *cStr = "a";
+    StrView sv;
+    strViewInitFromC(&sv, cStr);
+
+    testAssert(strcmp((char *)sv.buf, cStr) == 0);
+    testAssert(sv.len == 1);
+}
+
+testList(
+    testMake(test_strViewInitFromCEmpty),
+    testMake(test_strViewInitFromCFull)
+)
---
+++ tests/test_str/strViewMakeFromC.c
@@ -0,0 +1,25 @@
+#include <string.h>
+
+#include "nv_test.h"
+#include "nv_str.h"
+
+void test_strViewMakeFromCEmpty(void) {
+    const char *cStr = "";
+    StrView sv = strViewMakeFromC(cStr);
+
+    testAssert(strcmp((char *)sv.buf, cStr) == 0);
+    testAssert(sv.len == 0);
+}
+
+void test_strViewMakeFromCFull(void) {
+    const char *cStr = "a";
+    StrView sv = strViewMakeFromC(cStr);
+
+    testAssert(strcmp((char *)sv.buf, cStr) == 0);
+    testAssert(sv.len == 1);
+}
+
+testList(
+    testMake(test_strViewMakeFromCEmpty),
+    testMake(test_strViewMakeFromCFull)
+)
