766ffb8bdb09742234d8994a52bd951277e87493
TheSilvered <taffarello.da@gmail.com>
2025-08-15 22:56:23 +0200

    Correct detection of input sequences

--- src/nv_term.c
+++ src/nv_term.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include "nv_term.h"
 
 #ifdef _WIN32
@@ -10,55 +11,100 @@ bool termIsInit(void) {
     return g_initialized;
 }
 
-static UcdCP g_keyQueue[4] = { 0 };
+static UcdCP g_queuedKey = 0;
 
-static void queueKey(UcdCP key) {
-    if (g_keyQueue[0] == 0) {
-        g_keyQueue[0] = key;
-    } else if (g_keyQueue[1] == 0) {
-        g_keyQueue[1] = key;
-    } else if (g_keyQueue[2] == 0) {
-        g_keyQueue[2] = key;
-    } else {
-        g_keyQueue[3] = key;
-    }
-}
-
-static UcdCP getKey(void) {
-    if (g_keyQueue[0] != 0) {
-        UcdCP key = g_keyQueue[0];
-        g_keyQueue[0] = g_keyQueue[1];
-        g_keyQueue[1] = g_keyQueue[2];
-        g_keyQueue[2] = g_keyQueue[3];
-        g_keyQueue[3] = 0;
+static UcdCP getKey_(void) {
+    if (g_queuedKey != 0) {
+        UcdCP key = g_queuedKey;
+        g_queuedKey = 0;
         return key;
     } else {
         return termGetInput();
     }
 }
 
+static size_t readEscapeSeq_(UcdCP *outBuf, size_t bufSize) {
+    assert(bufSize >= 3);
+    UcdCP key = getKey_();
+    if (key == TermKey_None) {
+        return 0;
+    }
+
+    if (key != TermKey_Escape) {
+        outBuf[0] = key;
+        return 1;
+    }
+
+    key = getKey_();
+    if (key == TermKey_Escape) {
+        // ESC ESC is the same as a single ESC
+        outBuf[0] = key;
+        return 1;
+    }
+    if (key != '[' && key != 'O') {
+        outBuf[0] = TermKey_Escape;
+        g_queuedKey = key;
+        return 1;
+    }
+    // The initial ESC is omitted in the buffer.
+    outBuf[0] = key;
+
+    if (key == 'O') {
+        key = getKey_();
+        if (key == TermKey_None) {
+            return 0;
+        }
+        outBuf[1] = key;
+        return 2;
+    }
+
+    size_t i = 1;
+    for (;;) {
+        key = getKey_();
+        if (i < bufSize) {
+            outBuf[i++] = key;
+        }
+
+        if (key == TermKey_None) {
+            return 0;
+        }
+
+        if ((key >= '0' && key <= '9') || key == ';') {
+            continue;
+        }
+    }
+
+    // Ignore sequences too long for the buffer
+    return i < bufSize ? i : 0;
+}
+
 int termGetKey(void) {
-    UcdCP seq[4] = { 0 };
-    seq[0] = getKey();
-    if (seq[0] != TermKey_Escape) {
+#define SeqBufLen_ 32
+    UcdCP seq[SeqBufLen_] = { 0 };
+    size_t seqLen = readEscapeSeq_(seq, SeqBufLen_);
+#undef SeqBufLen_
+
+    if (seqLen == 0) {
+        return 0;
+    } else if (seqLen == 1) {
         return seq[0];
     }
 
-    seq[1] = getKey();
-    if (seq[1] != '[') {
-        queueKey(seq[1]);
-        return seq[0];
+    if (seq[0] == 'O') {
+        switch (seq[1]) {
+        case 'A': return TermKey_ArrowUp;
+        case 'B': return TermKey_ArrowDown;
+        case 'C': return TermKey_ArrowRight;
+        case 'D': return TermKey_ArrowLeft;
+        default: return 0;
+        }
     }
-
-    seq[3] = getKey();
-    switch (seq[3]) {
+    assert(seq[0] == '[');
+    switch (seq[1]) {
     case 'A': return TermKey_ArrowUp;
     case 'B': return TermKey_ArrowDown;
     case 'C': return TermKey_ArrowRight;
     case 'D': return TermKey_ArrowLeft;
-    default:
-        queueKey(seq[1]);
-        queueKey(seq[2]);
-        return seq[0];
+    default: return 0;
     }
 }
