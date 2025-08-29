23e21d5f4ee69c3597678a77719abb92525fc83c
TheSilvered <taffarello.da@gmail.com>
2025-08-30 00:45:34 +0200

    Added dynamic array

---
+++ include/nv_array.h
@@ -0,0 +1,58 @@
+#ifndef NV_ARRAY_H_
+#define NV_ARRAY_H_
+
+#include "nv_mem.h"
+
+// Dynamic array macros.
+// Define a dynamic array as a struct with the fields `items`, `len` and `cap`.
+// Items is required to be a pointer.
+
+#define arrReserve(arr, count)                                                 \
+    do {                                                                       \
+        if ((arr)->len + (count) > (arr)->cap) {                               \
+            (arr)->cap = (size_t)(((arr)->len + (count)) * 1.5);               \
+            (arr)->items = memExpand(                                          \
+                (arr)->items,                                                  \
+                (arr)->cap,                                                    \
+                sizeof(*(arr)->items)                                          \
+            );                                                                 \
+        }                                                                      \
+    } while (0)
+
+#define arrResize(arr, requiredLen)                                            \
+    do {                                                                       \
+        if (requiredLen == (arr)->len) {                                       \
+            break;                                                             \
+        } else if (requiredLen == 0) {                                         \
+            memFree((arr)->items);                                             \
+            (arr)->items = NULL;                                               \
+            (arr)->len = 0;                                                    \
+            (arr)->cap = 0;                                                    \
+        } else if (requiredLen < (arr)->cap / 4) {                             \
+            (arr)->items = memShrink(                                          \
+                (arr)->items,                                                  \
+                (arr)->cap / 2,                                                \
+                sizeof(*(arr)->items)                                          \
+            );                                                                 \
+            (arr)->cap /= 2;                                                   \
+        } else if (requiredLen > (arr)->cap) {                                 \
+            size_t newCap = requiredLen + requiredLen / 2;                     \
+            (arr)->items = memChange(                                          \
+                (arr)->items,                                                  \
+                newCap,                                                        \
+                sizeof(*(arr)->items)                                          \
+            );                                                                 \
+            (arr)->cap = newCap;                                               \
+        }                                                                      \
+    } while (0)
+
+
+#define arrAppend(arr, item)                                                   \
+    do {                                                                       \
+        arrReserve((arr), 1);                                                  \
+        (arr)->items[(arr)->len++] = (item);                                   \
+    } while (0)
+
+#define arrDestroy(arr) memFree((arr)->items)
+
+#endif // !NV_ARRAY_H_
