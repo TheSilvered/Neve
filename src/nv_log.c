05001980b9e6d448640f1742150d7fa990868f3a
TheSilvered <taffarello.da@gmail.com>
2025-09-05 11:48:48 +0200

    Log generates in standard location instead of cwd

--- src/nv_log.c
+++ src/nv_log.c
@@ -1,14 +1,43 @@
 #include <stdarg.h>
+#include <stdlib.h>
 #include "nv_log.h"
+#include "nv_utils.h"
 
 static File logFile = { 0 };
 
+#ifdef _WIN32
+
+bool logInit(const char *filePath) {
+    if (filePath =! NULL) {
+        return fileOpen(&logFile, filePath, FileMode_Write)
+            == FileIOResult_Success;
+    }
+
+    UcdCh8 buf[1024];
+    StrBuf sb = strBufMake(buf, NV_ARRLEN(buf));
+    const char *appdata = getenv("LOCALAPPDATA");
+    if (appdata == NULL) {
+        return false;
+    }
+
+    strBufAppendC(&sb, appdata);
+    if (sb.buf[sb.len - 1] != '\\' && sb.buf[sb.len - 1] != '/') {
+        strBufAppendC(&sb, "\\");
+    }
+    strBufAppendC(&sb, "neve.log.txt");
+    return fileOpen(&logFile, filePath, FileMode_Write) == FileIOResult_Success;
+}
+
+#else
+
 bool logInit(const char *filePath) {
     if (filePath == NULL) {
-        filePath = "log.txt";
+        filePath = "~/.neve.log.txt";
     }
     return fileOpen(&logFile, filePath, FileMode_Write) == FileIOResult_Success;
 }
+
+#endif
 
 void logQuit(void) {
     fileClose(&logFile);
