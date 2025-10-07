b7da6db19043434f476eee7c2e68aa7759e8a878
Davide Taffarello <taffarello.da@gmail.com>
2025-10-07 14:24:36 +0200

    Added more CtxBuf tests

---
+++ tests/test_nv_context/ctxBufRemove.c
@@ -0,0 +1,42 @@
+#include "nv_test.h"
+#include "nv_context.c"
+#include "nv_unicode.h"
+
+void test_ctxBufRemoveEmpty(void) {
+    CtxBuf buf = { 0 };
+
+    ctxBufRemove_(&buf, 1);
+    testAssert(buf.len == 0);
+    testAssert(buf.gapIdx == 0);
+}
+
+void test_ctxBufRemoveFromEnd(void) {
+    CtxBuf buf = { 0 };
+    const char s[] = "abcd";
+
+    ctxBufInsert_(&buf, (UcdCh8 *)s, chArrLen(s));
+
+    ctxBufRemove_(&buf, 2);
+    testAssert(buf.len == 2);
+    testAssert(*ctxBufGet_(&buf, 0) == 'a');
+    testAssert(*ctxBufGet_(&buf, 1) == 'b');
+}
+
+void test_ctxBufRemoveFromMiddle(void) {
+    CtxBuf buf = { 0 };
+    const char s[] = "abcd";
+
+    ctxBufInsert_(&buf, (UcdCh8 *)s, chArrLen(s));
+    ctxBufSetGapIdx_(&buf, 2);
+    ctxBufRemove_(&buf, 2);
+    testAssert(buf.len == 2);
+    testAssert(*ctxBufGet_(&buf, 0) == 'c');
+    testAssert(*ctxBufGet_(&buf, 1) == 'd');
+}
+
+testList(
+    testMake(test_ctxBufRemoveEmpty),
+    testMake(test_ctxBufRemoveFromEnd),
+    testMake(test_ctxBufRemoveFromMiddle)
+)
+
--- tests/test_nv_context/ctxBufShrink.c
+++ tests/test_nv_context/ctxBufShrink.c
@@ -25,8 +25,28 @@ void test_ctxBufShrinkEmpty(void) {
     memFree(buf.bytes);
 }
 
+void test_ctxShrkinkFull(void) {
+    CtxBuf buf = { 0 };
+    const char s[] = "abcd";
+
+    ctxBufReserve_(&buf, 1000);
+    ctxBufInsert_(&buf, (UcdCh8 *)s, chArrLen(s));
+    ctxBufSetGapIdx_(&buf, 2);
+
+    size_t prevCap = buf.cap;
+
+    ctxBufShrink_(&buf);
+
+    testAssert(buf.cap < prevCap);
+    testAssert(*ctxBufGet_(&buf, 0) == s[0]);
+    testAssert(*ctxBufGet_(&buf, 1) == s[1]);
+    testAssert(*ctxBufGet_(&buf, 2) == s[2]);
+    testAssert(*ctxBufGet_(&buf, 3) == s[3]);
+}
+
 testList(
     testMake(test_ctxBufShrinkNoEffect),
-    testMake(test_ctxBufShrinkEmpty)
+    testMake(test_ctxBufShrinkEmpty),
+    testMake(test_ctxShrkinkFull)
 )
 
