ea71908bc2bd8d13388eec959f054f8cb23759fb
TheSilvered <taffarello.da@gmail.com>
2025-12-04 17:38:14 +0100

    Added buffer tests

---
+++ tests/test_nv_buffer/bufNewEmpy.c
@@ -0,0 +1,24 @@
+#include "nv_test.h"
+#include "nv_buffer.c"
+
+void test_bufNewEmpty(void) {
+    BufMap map;
+    bufMapInit(&map);
+
+    BufHandle hd1 = bufNewEmpty(&map);
+    testAssert(hd1 != bufInvalidHandle);
+    BufHandle hd2 = bufNewEmpty(&map);
+    testAssert(hd2 != bufInvalidHandle);
+    testAssert(hd1 != hd2);
+
+    for (int i = 0; i < 100; i++) {
+        BufHandle hd = bufNewEmpty(&map);
+        testAssert(hd != bufInvalidHandle);
+    }
+
+    bufMapDestroy(&map);
+}
+
+testList(
+    testMake(test_bufNewEmpty)
+)
